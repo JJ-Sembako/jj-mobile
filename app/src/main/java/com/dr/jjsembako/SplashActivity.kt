033ec17a583b5a312874b3e7d8eddf6dd0276491
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-11-24 18:34:48 +0700

    reduce splash screen time

--- app/src/main/java/com/dr/jjsembako/SplashActivity.kt
+++ app/src/main/java/com/dr/jjsembako/SplashActivity.kt
@@ -24,7 +24,7 @@ class SplashActivity : ComponentActivity() {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 1250)
     }
 }
 
