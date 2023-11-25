4bd488ec10c9814a21c610a06449f9a56f484326
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-11-25 23:40:04 +0700

    Rename file and add few setting

--- app/src/main/java/com/dr/jjsembako/ui/feature_setting/change_password/GantiKataSandiScreen.kt
+++ app/src/main/java/com/dr/jjsembako/ui/feature_setting/change_password/GantiKataSandiScreen.kt
@@ -23,10 +23,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -107,6 +109,10 @@ fun GantiKataSandiScreen(
     Scaffold(
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
                 title = { Text(stringResource(R.string.ganti_kata_sandi)) },
                 navigationIcon = {
                     IconButton(onClick = { onNavigateToSetting() }) {
@@ -127,7 +133,7 @@ fun GantiKataSandiScreen(
                     indication = null,
                     interactionSource = remember { MutableInteractionSource() },
                     onClick = { keyboardController?.hide() })
-                .padding(contentPadding),
+                .padding(contentPadding).padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Column(
