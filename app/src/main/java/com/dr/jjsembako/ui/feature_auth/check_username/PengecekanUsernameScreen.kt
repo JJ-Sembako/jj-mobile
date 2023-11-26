e5114f53e348b96533387709ba232772e445a086
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-11-26 10:49:31 +0700

    Add form

--- app/src/main/java/com/dr/jjsembako/ui/feature_auth/check_username/PengecekanUsernameScreen.kt
+++ app/src/main/java/com/dr/jjsembako/ui/feature_auth/check_username/PengecekanUsernameScreen.kt
@@ -1,30 +1,81 @@
 package com.dr.jjsembako.ui.feature_auth.check_username
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dr.jjsembako.R
+import com.dr.jjsembako.core.utils.isValidUsername
 import com.dr.jjsembako.ui.theme.JJSembakoTheme
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun PengecekanUsernameScreen(
     onNavigateToLogin: () -> Unit, onNavigateToCheckAnswer: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    val keyboardHeight = WindowInsets.ime.getBottom(LocalDensity.current)
+
+    LaunchedEffect(key1 = keyboardHeight) {
+        coroutineScope.launch {
+            scrollState.scrollBy(keyboardHeight.toFloat())
+        }
+    }
+
+    var username by rememberSaveable { mutableStateOf("") }
+    var isValidUsername = rememberSaveable { mutableStateOf(false) }
+    var errMsgUsername = rememberSaveable { mutableStateOf("") }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -44,8 +95,63 @@ fun PengecekanUsernameScreen(
             )
         }
     ) { contentPadding ->
-        Column(modifier = modifier.padding(contentPadding)) {
-            Text(text = "Cek Username")
+        Column(
+            modifier = modifier
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = { keyboardController?.hide() })
+                .padding(contentPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                label = { Text(stringResource(R.string.username)) },
+                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                leadingIcon = { Icon(Icons.Default.Person, contentDescription = null) },
+                isError = !isValidUsername.value,
+                value = username,
+                onValueChange = {
+                    username = it
+                    if (!isValidUsername(username)) {
+                        isValidUsername.value = false
+                        errMsgUsername.value =
+                            "Username minimal 5 karakter diawali huruf diikuti karakter huruf, angka, underscore, atau strip!"
+                    } else {
+                        isValidUsername.value = true
+                        errMsgUsername.value = ""
+                    }
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+                text = errMsgUsername.value,
+                fontSize = 14.sp,
+                color = Color.Red
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { onNavigateToCheckAnswer() },
+                enabled = isValidUsername.value,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+            ) {
+                Text(stringResource(R.string.periksa))
+            }
         }
     }
 }
