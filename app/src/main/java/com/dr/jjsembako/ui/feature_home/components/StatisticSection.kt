d70c833a59221d442e2e5a63450daebe14b687fd
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-01 21:17:50 +0700

    Cleaning StatisticSection

--- app/src/main/java/com/dr/jjsembako/ui/feature_home/components/StatisticSection.kt
+++ app/src/main/java/com/dr/jjsembako/ui/feature_home/components/StatisticSection.kt
@@ -13,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,7 +57,8 @@ private fun StatisticCard(total: Int, isPesanan: Boolean, modifier: Modifier) {
                 text = text,
                 textAlign = TextAlign.Center,
                 modifier = modifier.padding(bottom = 8.dp),
-                fontSize = 14.sp
+                fontSize = 14.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
             )
             Text(
                 text = formatTotal(total),
