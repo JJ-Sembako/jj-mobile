185be14451eaed1f2b5544407de86d8d4cdce7ed
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-06 17:47:13 +0700

    make loading dialog component

--- app/src/main/java/com/dr/jjsembako/core/presentation/components/LoadingDialog.kt
+++ app/src/main/java/com/dr/jjsembako/core/presentation/components/LoadingDialog.kt
@@ -1,21 +1,31 @@
 package com.dr.jjsembako.core.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,16 +41,22 @@ fun LoadingDialog(
     val dialogMinWidth = 280.dp
     val dialogMaxWidth = 560.dp
 
-    Dialog(onDismissRequest = { showDialog.value = false }) {
-        Card(
+    Dialog(
+        onDismissRequest = { showDialog.value = false }) {
+        Column(
             modifier = modifier
                 .sizeIn(
-                    minWidth = dialogMinWidth, maxWidth = dialogMaxWidth
+                    minWidth = dialogMinWidth,
+                    maxWidth = dialogMaxWidth
                 )
                 .height(300.dp)
-                .padding(24.dp),
-            shape = RoundedCornerShape(16.dp)
+                .padding(24.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.surfaceVariant),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Spacer(modifier = modifier.height(16.dp))
             CircularProgressIndicator(
                 modifier = modifier.size(100.dp)
             )
@@ -49,12 +65,16 @@ fun LoadingDialog(
                 text = stringResource(R.string.on_progress),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = modifier.wrapContentSize(Alignment.Center)
             )
             Spacer(modifier = modifier.height(16.dp))
             Text(
                 text = stringResource(R.string.please_wait),
                 fontWeight = FontWeight.Medium,
                 fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = modifier.wrapContentSize(Alignment.Center)
             )
             Spacer(modifier = modifier.height(24.dp))
         }
