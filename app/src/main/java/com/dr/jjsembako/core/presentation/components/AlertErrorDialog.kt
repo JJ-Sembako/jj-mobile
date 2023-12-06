3fb28d55058ea9b90ea69c2dbbb8253fa0525734
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-06 22:16:32 +0700

    update loading dialog component & made error dialog

--- app/src/main/java/com/dr/jjsembako/core/presentation/components/AlertErrorDialog.kt
+++ app/src/main/java/com/dr/jjsembako/core/presentation/components/AlertErrorDialog.kt
@@ -2,18 +2,19 @@ package com.dr.jjsembako.core.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ErrorOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -34,9 +36,10 @@ import com.dr.jjsembako.R
 import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
 
 @Composable
-fun LoadingDialog(
+fun AlertErrorDialog(
+    message: String = "",
     showDialog: MutableState<Boolean>,
-    modifier: Modifier
+    modifier: Modifier,
 ) {
     val dialogMinWidth = 280.dp
     val dialogMaxWidth = 560.dp
@@ -55,13 +58,15 @@ fun LoadingDialog(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = modifier.height(16.dp))
-            CircularProgressIndicator(
-                modifier = modifier.size(100.dp)
+            Icon(
+                imageVector = Icons.Default.ErrorOutline,
+                contentDescription = stringResource(R.string.error),
+                tint = Color.Red,
+                modifier = modifier.size(80.dp)
             )
             Spacer(modifier = modifier.height(16.dp))
             Text(
-                text = stringResource(R.string.on_progress),
+                text = stringResource(R.string.error),
                 fontWeight = FontWeight.Medium,
                 fontSize = 20.sp,
                 textAlign = TextAlign.Center,
@@ -69,22 +74,41 @@ fun LoadingDialog(
             )
             Spacer(modifier = modifier.height(16.dp))
             Text(
-                text = stringResource(R.string.please_wait),
+                text = message,
                 fontWeight = FontWeight.Normal,
                 fontSize = 14.sp,
                 textAlign = TextAlign.Center,
                 modifier = modifier.wrapContentSize(Alignment.Center)
             )
             Spacer(modifier = modifier.height(24.dp))
+            Button(
+                onClick = {
+                    showDialog.value = false
+                },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Red,
+                    containerColor = Color.Red
+                ),
+                shape = RoundedCornerShape(50.dp),
+                modifier = modifier.padding(horizontal = 5.dp, vertical = 10.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.ok),
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    modifier = modifier.padding(horizontal = 4.dp, vertical = 4.dp)
+                )
+            }
         }
     }
 }
 
 @Composable
 @Preview(showBackground = true)
-fun LoadingDialogPreview() {
+fun AlertErrorDialogPreview() {
     JJSembakoTheme {
-        LoadingDialog(
+        AlertErrorDialog(
+            message = "Akun tidak ditemukan.",
             showDialog = remember { mutableStateOf(true) },
             modifier = Modifier
         )
