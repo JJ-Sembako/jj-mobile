628d255a29b3bae383dd5f7cfbea1115070b0a1a
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-11 08:12:34 +0700

    update ICustomerRepository & add data source

--- app/src/main/java/com/dr/jjsembako/feature_customer/data/CustomerDataSource.kt
+++ app/src/main/java/com/dr/jjsembako/feature_customer/data/CustomerDataSource.kt
@@ -2,10 +2,190 @@ package com.dr.jjsembako.feature_customer.data
 
 import com.dr.jjsembako.core.common.Resource
 import com.dr.jjsembako.core.data.remote.network.CustomerApiService
+import com.dr.jjsembako.core.data.remote.response.account.PostHandleLoginResponse
 import com.dr.jjsembako.core.data.remote.response.customer.DataCustomer
+import com.dr.jjsembako.core.data.remote.response.customer.DeleteHandleDeleteCustomerResponse
+import com.google.gson.Gson
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import okio.IOException
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class CustomerDataSource @Inject constructor(private val customerApiService: CustomerApiService) {
+
+    suspend fun fetchCustomers(
+        search: String? = null,
+        page: Int? = null,
+        limit: Int? = null
+    ): Flow<Resource<out List<DataCustomer?>>> = flow {
+        try {
+            val response = customerApiService.fetchCustomers(search, page, limit)
+            emit(Resource.Success(response.data, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        Gson().fromJson(errorResponse, PostHandleLoginResponse::class.java)
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun handleCreateCustomer(
+        name: String,
+        shopName: String,
+        address: String,
+        gmapsLink: String,
+        phoneNumber: String
+    ): Flow<Resource<out DataCustomer?>> = flow {
+        try {
+            val response = customerApiService.handleCreateCustomer(
+                name,
+                shopName,
+                address,
+                gmapsLink,
+                phoneNumber
+            )
+            emit(Resource.Success(response.data, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        Gson().fromJson(errorResponse, PostHandleLoginResponse::class.java)
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun fetchDetailCustomer(id: String): Flow<Resource<out DataCustomer?>> = flow {
+        try {
+            val response = customerApiService.fetchDetailCustomer(id)
+            emit(Resource.Success(response.data, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        Gson().fromJson(errorResponse, PostHandleLoginResponse::class.java)
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun handleUpdateCustomer(
+        id: String,
+        name: String,
+        shopName: String,
+        address: String,
+        gmapsLink: String,
+        phoneNumber: String
+    ): Flow<Resource<out DataCustomer?>> = flow {
+        try {
+            val response = customerApiService.handleUpdateCustomer(
+                id,
+                name,
+                shopName,
+                address,
+                gmapsLink,
+                phoneNumber
+            )
+            emit(Resource.Success(response.data, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        Gson().fromJson(errorResponse, PostHandleLoginResponse::class.java)
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun handleDeleteCustomer(id: String): Flow<Resource<out DeleteHandleDeleteCustomerResponse>> =
+        flow {
+            try {
+                val response = customerApiService.handleDeleteCustomer(id)
+                emit(Resource.Success(null, response.message, response.statusCode))
+            } catch (e: CancellationException) {
+                // Do nothing, the flow is cancelled
+            } catch (e: IOException) {
+                // Handle IOException (connection issues, timeout, etc.)
+                emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+            } catch (e: Exception) {
+                if (e is HttpException) {
+                    val errorResponse = e.response()?.errorBody()?.string()
+                    val statusCode = e.code()
+                    val errorMessage = e.message()
+
+                    if (errorResponse != null) {
+                        val errorResponseObj =
+                            Gson().fromJson(errorResponse, PostHandleLoginResponse::class.java)
+                        emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                    } else {
+                        emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                    }
+                } else {
+                    emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+                }
+            }
+        }.flowOn(Dispatchers.IO)
 
 }
--- app/src/main/java/com/dr/jjsembako/feature_customer/domain/repository/ICustomerRepository.kt
+++ app/src/main/java/com/dr/jjsembako/feature_customer/domain/repository/ICustomerRepository.kt
@@ -3,8 +3,6 @@ package com.dr.jjsembako.feature_customer.domain.repository
 import com.dr.jjsembako.core.common.Resource
 import com.dr.jjsembako.core.data.remote.response.customer.DataCustomer
 import com.dr.jjsembako.core.data.remote.response.customer.DeleteHandleDeleteCustomerResponse
-import com.dr.jjsembako.core.data.remote.response.customer.PostHandleCreateCustomerResponse
-import com.dr.jjsembako.core.data.remote.response.customer.PutHandleUpdateCustomerResponse
 import kotlinx.coroutines.flow.Flow
 
 interface ICustomerRepository {
@@ -21,7 +19,7 @@ interface ICustomerRepository {
         address: String,
         gmapsLink: String,
         phoneNumber: String
-    ): Flow<Resource<PostHandleCreateCustomerResponse>>
+    ): Flow<Resource<out DataCustomer?>>
 
     suspend fun fetchDetailCustomer(id: String): Flow<Resource<out DataCustomer?>>
 
@@ -32,7 +30,7 @@ interface ICustomerRepository {
         address: String,
         gmapsLink: String,
         phoneNumber: String
-    ): Flow<Resource<PutHandleUpdateCustomerResponse>>
+    ): Flow<Resource<out DataCustomer?>>
 
-    suspend fun handleDeleteCustomer(id: String): Flow<Resource<DeleteHandleDeleteCustomerResponse>>
+    suspend fun handleDeleteCustomer(id: String): Flow<Resource<out DeleteHandleDeleteCustomerResponse>>
 }
