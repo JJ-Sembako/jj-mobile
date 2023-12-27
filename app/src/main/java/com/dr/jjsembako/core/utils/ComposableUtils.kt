7896039139ef9fe07f2c93741b93282cbcc566ba
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-27 08:44:09 +0700

    add rememberMutableStateMapOf

--- app/src/main/java/com/dr/jjsembako/core/utils/ComposableUtils.kt
+++ app/src/main/java/com/dr/jjsembako/core/utils/ComposableUtils.kt
@@ -1,10 +1,12 @@
 package com.dr.jjsembako.core.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.runtime.toMutableStateMap
 
 @Composable
 fun <T : Any> rememberMutableStateListOf(vararg elements: T): SnapshotStateList<T> {
@@ -25,3 +27,26 @@ fun <T : Any> rememberMutableStateListOf(vararg elements: T): SnapshotStateList<
         elements.toList().toMutableStateList()
     }
 }
+
+@Composable
+fun <K, V> rememberMutableStateMapOf(vararg pairs: Pair<K, V>)  =
+    rememberSaveable(
+        saver = listSaver(
+            save = { it.toList() },
+            restore = { it.toMutableStateMap() }
+        )
+    ) {
+        pairs.toList().toMutableStateMap()
+    }
+
+@Composable
+fun <K, V> rememberMutableStateMapOf(defaultValue: V, vararg inputs: Any?)  =
+    rememberSaveable(
+        inputs = inputs,
+        saver = listSaver(
+            save = { it.toList() },
+            restore = { it.toMutableStateMap().withDefault { defaultValue } }
+        )
+    ) {
+        mutableStateMapOf<K, V>().withDefault { defaultValue }
+    }
