260d5848940c3af7994efadb06395597e783fc23
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-28 22:24:01 +0700

    create basic RiwayatScreen

---
+++ app/src/main/java/com/dr/jjsembako/feature_history/presentation/list/RiwayatScreen.kt
@@ -0,0 +1,116 @@
+package com.dr.jjsembako.feature_history.presentation.list
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.dr.jjsembako.R
+import com.dr.jjsembako.core.presentation.components.SearchFilter
+import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun RiwayatScreen(onNavigateBack: () -> Unit, modifier: Modifier = Modifier) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    var showSheet = remember { mutableStateOf(false) }
+    var searchQuery = rememberSaveable { mutableStateOf("") }
+    var activeSearch = remember { mutableStateOf(false) }
+
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.anim_empty))
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+    )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
+                title = { Text(stringResource(R.string.history)) },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        keyboardController?.hide()
+                        onNavigateBack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back),
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                }
+            )
+        }
+    ) { contentPadding ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = {
+                        keyboardController?.hide()
+                        activeSearch.value = false
+                        focusManager.clearFocus()
+                    })
+                .padding(contentPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SearchFilter(
+                placeholder = stringResource(R.string.search_transaction),
+                activeSearch,
+                searchQuery,
+                searchFunction = { },
+                openFilter = { showSheet.value = !showSheet.value },
+                modifier = modifier
+            )
+            Spacer(modifier = modifier.height(16.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RiwayatScreenPreview() {
+    JJSembakoTheme {
+        RiwayatScreen(
+            onNavigateBack = {}
+        )
+    }
+}
