b6d2edf868ada7b940e0955ed1b0c7a315a0514e
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-12-29 10:19:16 +0700

    create switchFilter date

--- app/src/main/java/com/dr/jjsembako/feature_history/presentation/components/BottomSheetHistory.kt
+++ app/src/main/java/com/dr/jjsembako/feature_history/presentation/components/BottomSheetHistory.kt
@@ -1,7 +1,9 @@
 package com.dr.jjsembako.feature_history.presentation.components
 
+import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,6 +14,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -28,6 +31,12 @@ import androidx.compose.ui.unit.sp
 import com.dr.jjsembako.R
 import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -78,6 +87,42 @@ fun BottomSheetHistory(
             }
         }
     )
+}
+
+@Composable
+private fun SwitchFilter(
+    fromDate: MutableState<String>,
+    untilDate: MutableState<String>,
+    isFilterOn: MutableState<Boolean>,
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(stringResource(id = R.string.activate_account_recovery))
+        Switch(checked = isFilterOn.value, onCheckedChange = {
+            isFilterOn.value = it
+            if (isFilterOn.value) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    fromDate.value = LocalDate.now().withDayOfMonth(1)
+                        .format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                    untilDate.value =
+                        LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                } else {
+                    val calendar = Calendar.getInstance()
+                    calendar.set(Calendar.DAY_OF_MONTH, 1)
+                    val firstDayOfMonth = calendar.time
+                    fromDate.value =
+                        SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(firstDayOfMonth)
+                    untilDate.value =
+                        SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+                }
+            }
+        })
+    }
 }
 
 @Composable
