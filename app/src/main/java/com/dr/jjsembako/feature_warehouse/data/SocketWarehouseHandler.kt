c5ad912ba55d2c61f4db013c0b927233e911cffd
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-04 09:41:31 +0700

    create Socket Warehouse Handler

---
+++ app/src/main/java/com/dr/jjsembako/feature_warehouse/data/SocketWarehouseHandler.kt
@@ -0,0 +1,98 @@
+package com.dr.jjsembako.feature_warehouse.data
+
+import android.content.SharedPreferences
+import android.util.Log
+import com.dr.jjsembako.BuildConfig
+import com.dr.jjsembako.core.data.remote.response.product.DataProduct
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import io.socket.client.IO
+import io.socket.client.Socket
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SocketWarehouseHandler @Inject constructor(
+    private val sharedPreferences: SharedPreferences,
+    private val gson: Gson
+) {
+    companion object {
+        private const val TAG = "Socket-Product"
+    }
+
+    private lateinit var socket: Socket
+
+    // Callbacks for events
+    var onProductsReceived: ((List<DataProduct>) -> Unit)? = null
+    var onNewProductReceived: ((DataProduct) -> Unit)? = null
+    var onUpdateProductReceived: ((List<DataProduct>) -> Unit)? = null
+    var onDeleteProductReceived: ((String) -> Unit)? = null
+    var onErrorReceived: ((String) -> Unit)? = null
+    var onLoadingState: ((Boolean) -> Unit)? = null
+
+    fun connect() {
+        val token = sharedPreferences.getString("token", "")
+        val options = IO.Options().apply {
+            extraHeaders = mapOf("Authorization" to listOf("Bearer $token"))
+            path = "product"
+            transports = arrayOf("websocket")
+            reconnection = true
+            reconnectionDelay = 1000
+            reconnectionDelayMax = Integer.MAX_VALUE.toLong()
+            reconnectionAttempts = 99999
+        }
+        val socket = IO.socket(BuildConfig.WS_URL, options)
+
+        socket.on(Socket.EVENT_CONNECT) {
+            Log.d(TAG, "Socket connected")
+            onLoadingState?.invoke(true)
+        }
+
+        socket.on(Socket.EVENT_CONNECT_ERROR) {
+            Log.e(TAG, "Socket connection error")
+            onErrorReceived?.invoke("Gagal terhubung ke server!")
+            onLoadingState?.invoke(false)
+        }
+
+        socket.on(Socket.EVENT_DISCONNECT) {
+            Log.d(TAG, "Socket disconnected")
+            onErrorReceived?.invoke("Koneksi ke server terputus!")
+            onLoadingState?.invoke(false)
+        }
+
+        socket.on("products") { args ->
+            val products = gson.fromJson<List<DataProduct>>(
+                args[0].toString(),
+                object : TypeToken<List<DataProduct>>() {}.type
+            )
+            onProductsReceived?.invoke(products)
+            onLoadingState?.invoke(false)
+        }
+
+        socket.on("new-product") { args ->
+            val product = gson.fromJson<DataProduct>(args[0].toString(), DataProduct::class.java)
+            onNewProductReceived?.invoke(product)
+        }
+
+        socket.on("update-product") { args ->
+            val products = gson.fromJson<List<DataProduct>>(
+                args[0].toString(),
+                object : TypeToken<List<DataProduct>>() {}.type
+            )
+            onUpdateProductReceived?.invoke(products)
+        }
+
+        socket.on("delete-product") { args ->
+            val productId = args[0].toString()
+            onDeleteProductReceived?.invoke(productId)
+        }
+
+        this.socket = socket
+        socket.connect()
+    }
+
+    fun disconnect() {
+        socket.disconnect()
+    }
+
+}
