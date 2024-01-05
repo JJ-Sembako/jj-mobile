37063d3601a22ff115381d51da6973d697269592
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-05 08:42:04 +0700

    finally can use socket.io

--- app/src/main/java/com/dr/jjsembako/feature_warehouse/data/SocketWarehouseHandler.kt
+++ app/src/main/java/com/dr/jjsembako/feature_warehouse/data/SocketWarehouseHandler.kt
@@ -8,6 +8,13 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import io.socket.client.IO
 import io.socket.client.Socket
+import io.socket.engineio.client.transports.Polling
+import io.socket.engineio.client.transports.WebSocket
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+import java.util.logging.Level
+import java.util.logging.Logger
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -30,18 +37,34 @@ class SocketWarehouseHandler @Inject constructor(
     var onErrorReceived: ((String) -> Unit)? = null
     var onLoadingState: ((Boolean) -> Unit)? = null
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS))
+        .readTimeout(5000, TimeUnit.SECONDS)
+        .writeTimeout(5000, TimeUnit.SECONDS)
+        .build()
+
     fun connect() {
         val token = sharedPreferences.getString("token", "")
         val options = IO.Options().apply {
             extraHeaders = mapOf("Authorization" to listOf("Bearer $token"))
-            path = "/ws/product"
-            transports = arrayOf("websocket")
+//            path = "/ws/product"
+            transports = arrayOf(WebSocket.NAME)
             reconnection = true
             reconnectionDelay = 1000
             reconnectionDelayMax = Integer.MAX_VALUE.toLong()
             reconnectionAttempts = 99999
         }
-        val socket = IO.socket("http://54.251.20.182:3000", options)
+        try {
+            IO.setDefaultOkHttpWebSocketFactory(client)
+            IO.setDefaultOkHttpCallFactory(client)
+
+            val socket = IO.socket("http://54.251.20.182:3000/ws/product", options)
+            this.socket = socket
+            socket.connect()
+
+        } catch (e: Exception) {
+            Log.e(TAG, "Error: ${e.message}")
+        }
 
         socket.on(Socket.EVENT_CONNECT) {
             Log.d(TAG, "Socket connected")
@@ -51,6 +74,7 @@ class SocketWarehouseHandler @Inject constructor(
         socket.on(Socket.EVENT_CONNECT_ERROR) {
             Log.e(TAG, "Socket connection error")
             Log.e(TAG, "Cause error: ${it[0]}")
+            Log.e(TAG, "Detail error: ${it.contentToString()}")
             onErrorReceived?.invoke("Gagal terhubung ke server!")
             onLoadingState?.invoke(false)
         }
@@ -87,9 +111,6 @@ class SocketWarehouseHandler @Inject constructor(
             val productId = args[0].toString()
             onDeleteProductReceived?.invoke(productId)
         }
-
-        this.socket = socket
-        socket.connect()
     }
 
     fun disconnect() {
