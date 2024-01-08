01f81ea80db780a2e8fc59313072e1539ee3da54
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-08 09:59:56 +0700

    add category use case conf

--- app/src/main/java/com/dr/jjsembako/feature_warehouse/domain/usecase/FetchCategoriesInteractor.kt
+++ app/src/main/java/com/dr/jjsembako/feature_warehouse/domain/usecase/FetchCategoriesInteractor.kt
@@ -1,4 +1,9 @@
 package com.dr.jjsembako.feature_warehouse.domain.usecase
 
-class FetchCategoriesInteractor {
+import com.dr.jjsembako.feature_warehouse.domain.repository.IWarehouseRepository
+import javax.inject.Inject
+
+class FetchCategoriesInteractor @Inject constructor(private val warehouseRepository: IWarehouseRepository) :
+    FetchCategoriesUseCase {
+    override suspend fun fetchCategories() = warehouseRepository.fetchCategories()
 }
