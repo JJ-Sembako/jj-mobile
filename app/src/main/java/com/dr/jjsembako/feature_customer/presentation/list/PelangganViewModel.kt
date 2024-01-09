9e5a284b5f509a5106350818dd40bd02e07eadc8
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-09 14:40:10 +0700

    cleaning code on presentation:customer_list

--- app/src/main/java/com/dr/jjsembako/feature_customer/presentation/list/PelangganViewModel.kt
+++ app/src/main/java/com/dr/jjsembako/feature_customer/presentation/list/PelangganViewModel.kt
@@ -1,12 +1,8 @@
 package com.dr.jjsembako.feature_customer.presentation.list
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
-import com.dr.jjsembako.core.common.Resource
-import com.dr.jjsembako.core.common.StateResponse
 import com.dr.jjsembako.core.data.remote.response.customer.DataCustomer
 import com.dr.jjsembako.feature_customer.domain.usecase.FetchCustomersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,7 +14,8 @@ import javax.inject.Inject
 class PelangganViewModel @Inject constructor(private val fetchCustomersUseCase: FetchCustomersUseCase) :
     ViewModel() {
 
-    private val _customerState: MutableStateFlow<PagingData<DataCustomer>> = MutableStateFlow(value = PagingData.empty())
+    private val _customerState: MutableStateFlow<PagingData<DataCustomer>> =
+        MutableStateFlow(value = PagingData.empty())
     val customerState: MutableStateFlow<PagingData<DataCustomer>> get() = _customerState
 
     fun fetchCustomers(searchQuery: String = "") {
