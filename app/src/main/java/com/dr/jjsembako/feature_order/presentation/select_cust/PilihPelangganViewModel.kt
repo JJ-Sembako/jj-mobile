80d26d1677c8b00b63836bed02122961ad09a12b
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-13 21:08:08 +0700

    create viewmodel

---
+++ app/src/main/java/com/dr/jjsembako/feature_order/presentation/select_cust/PilihPelangganViewModel.kt
@@ -0,0 +1,29 @@
+package com.dr.jjsembako.feature_order.presentation.select_cust
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.dr.jjsembako.core.data.model.DataCustomerOrder
+import com.dr.jjsembako.feature_order.domain.usecase.FetchSelectCustUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PilihPelangganViewModel @Inject constructor(private val fetchSelectCustUseCase: FetchSelectCustUseCase) :
+    ViewModel() {
+
+    private val _customerState: MutableStateFlow<PagingData<DataCustomerOrder>> =
+        MutableStateFlow(value = PagingData.empty())
+    val customerState: MutableStateFlow<PagingData<DataCustomerOrder>> get() = _customerState
+
+    fun fetchCustomers(searchQuery: String = "") {
+        viewModelScope.launch {
+            fetchSelectCustUseCase.fetchCustomers(searchQuery).collect {
+                _customerState.value = it
+            }
+        }
+    }
+
+}
