03fa9747d6b32dce593900e73c0578c9aef86fc6
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-13 20:58:03 +0700

    create paging and mapper customer

--- app/src/main/java/com/dr/jjsembako/core/utils/DataMapper.kt
+++ app/src/main/java/com/dr/jjsembako/core/utils/DataMapper.kt
@@ -1,6 +1,8 @@
 package com.dr.jjsembako.core.utils
 
+import com.dr.jjsembako.core.data.model.DataCustomerOrder
 import com.dr.jjsembako.core.data.model.FilterOption
+import com.dr.jjsembako.core.data.remote.response.customer.DataCustomer
 
 object DataMapper {
     fun mapListDataCategoryToListFilterOption(data: List<String?>?): List<FilterOption?> {
@@ -12,6 +14,29 @@ object DataMapper {
                     FilterOption(
                         name = category,
                         value = category
+                    )
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    fun mapListCustDataToListCustDataOrder(data: List<DataCustomer?>?): List<DataCustomerOrder> {
+        return if (data.isNullOrEmpty()) {
+            emptyList()
+        } else {
+            data.mapNotNull { cust ->
+                if (cust != null) {
+                    DataCustomerOrder(
+                        id = cust.id,
+                        name = cust.name,
+                        shopName = cust.shopName,
+                        address = cust.address,
+                        gmapsLink = cust.gmapsLink,
+                        phoneNumber = cust.phoneNumber,
+                        debt = cust.debt,
+                        isChosen = false
                     )
                 } else {
                     null
---
+++ app/src/main/java/com/dr/jjsembako/feature_order/data/OrderRepository.kt
@@ -0,0 +1,24 @@
+package com.dr.jjsembako.feature_order.data
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.dr.jjsembako.core.data.model.DataCustomerOrder
+import com.dr.jjsembako.feature_order.domain.repository.ISelectCustRepository
+import com.google.gson.Gson
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class OrderRepository @Inject constructor(
+    private val selectCustPagingSource: SelectCustPagingSource,
+    private val gson: Gson
+) : ISelectCustRepository {
+
+    override suspend fun fetchCustomers(searchQuery: String): Flow<PagingData<DataCustomerOrder>> {
+        return Pager(
+            config = PagingConfig(pageSize = 5),
+            pagingSourceFactory = { selectCustPagingSource.apply { setSearchQuery(searchQuery) } }
+        ).flow
+    }
+
+}
