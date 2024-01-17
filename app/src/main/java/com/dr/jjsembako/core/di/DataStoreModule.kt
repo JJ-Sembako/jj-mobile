ff8264bc4c59f9235dfb7b42a9038cbf5c08a50c
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-17 21:16:08 +0700

    create data store module

--- app/src/main/java/com/dr/jjsembako/core/di/DataStoreModule.kt
+++ app/src/main/java/com/dr/jjsembako/core/di/DataStoreModule.kt
@@ -2,27 +2,83 @@ package com.dr.jjsembako.core.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.dataStoreFile
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.dr.jjsembako.ProductOrderList
+import com.dr.jjsembako.core.utils.ProductOrderStoreSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataStoreModule {
-//    companion object {
-//        val PREFS_NAME = "order"
-//        val CUSTOMER_KEY = "customer"
-//        val PAYMENT_KEY = "payment"
-//        val PRODUCT_KEY = "product"
-//    }
-//
-//    @Singleton
-//    @Provides
-//    fun provideDataStore(@ApplicationContext context: Context) : DataStore<Preferences> {
-//        context.createDataStore(name = "order_data")
-//    }
+object DataStoreModule {
+
+    private const val ORDER_PREFERENCES = "order_preferences"
+    private const val DATA_STORE_FILE_NAME = "ordered_product_prefs.pb"
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { appContext.preferencesDataStoreFile(ORDER_PREFERENCES) }
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideProtoDataStore(@ApplicationContext appContext: Context): DataStore<ProductOrderList> {
+        return DataStoreFactory.create(
+            serializer = ProductOrderStoreSerializer(),
+            produceFile = { appContext.dataStoreFile(DATA_STORE_FILE_NAME) },
+            corruptionHandler = null,
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideIdCustomerPreference(dataStore: DataStore<Preferences>): Flow<String> {
+        return dataStore.data.map { preferences ->
+            preferences[PreferencesKeys.ID_CUSTOMER] ?: ""
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun providePaymentPreference(dataStore: DataStore<Preferences>): Flow<Boolean> {
+        return dataStore.data.map { preferences ->
+            preferences[PreferencesKeys.PAYMENT] ?: false
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideProductsList(dataStore: DataStore<ProductOrderList>): Flow<ProductOrderList> {
+        return dataStore.data
+    }
+
+    private object PreferencesKeys {
+        val ID_CUSTOMER = stringPreferencesKey("id_customer")
+        val PAYMENT = booleanPreferencesKey("payment")
+    }
 }
---
+++ app/src/main/java/com/dr/jjsembako/feature_order/presentation/create_order/BuatPesananViewModel.kt
@@ -0,0 +1,9 @@
+package com.dr.jjsembako.feature_order.presentation.create_order
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BuatPesananViewModel @Inject constructor() : ViewModel() {
+}
