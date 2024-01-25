79195de7941eabf84dc604d0ed2946262296db3c
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-25 14:32:16 +0700

    create OrderTimestamps component

---
+++ app/src/main/java/com/dr/jjsembako/feature_history/presentation/components/OrderTimestamps.kt
@@ -0,0 +1,112 @@
+package com.dr.jjsembako.feature_history.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dr.jjsembako.R
+import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
+
+@Composable
+fun OrderTimestamps(modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Divider(
+            modifier = modifier
+                .fillMaxWidth()
+                .width(1.dp), color = MaterialTheme.colorScheme.tertiary
+        )
+        Spacer(modifier = modifier.height(8.dp))
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.time_order_created), fontSize = 12.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+            Text(
+                text = "04-03-2023 15:25", fontSize = 12.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+        }
+        Spacer(modifier = modifier.height(8.dp))
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.time_order_delivered), fontSize = 12.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+            Text(
+                text = "06-03-2023 18:41", fontSize = 12.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+        }
+        Spacer(modifier = modifier.height(8.dp))
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.time_order_finished), fontSize = 12.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+            Text(
+                text = "07-03-2023 17:11", fontSize = 12.sp,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+            )
+        }
+        Spacer(modifier = modifier.height(8.dp))
+        Divider(
+            modifier = modifier
+                .fillMaxWidth()
+                .width(1.dp), color = MaterialTheme.colorScheme.tertiary
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun OrderTimestampsPreview() {
+    JJSembakoTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+        ) {
+            OrderTimestamps(
+                modifier = Modifier
+            )
+        }
+    }
+}
