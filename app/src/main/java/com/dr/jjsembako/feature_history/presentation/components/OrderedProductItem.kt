549d574678642a61028e1bd7457debff76b52a77
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-26 08:37:49 +0700

    create OrderedProductItem component

---
+++ app/src/main/java/com/dr/jjsembako/feature_history/presentation/components/OrderedProductItem.kt
@@ -0,0 +1,138 @@
+package com.dr.jjsembako.feature_history.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.dr.jjsembako.R
+import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
+import com.dr.jjsembako.core.utils.formatRupiah
+
+@Composable
+fun OrderedProductItem(
+    modifier: Modifier
+) {
+    OutlinedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .padding(horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            ProductImage(modifier = modifier)
+            OrderedProductInfo(modifier = modifier)
+        }
+    }
+}
+
+@Composable
+private fun ProductImage(
+    modifier: Modifier
+) {
+    val image = ""
+    val name = "Sari Roti Tawar"
+
+    if (image.isEmpty() || image.contains("default")) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_default),
+            contentDescription = stringResource(R.string.product_description, name),
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .padding(8.dp)
+                .width(60.dp)
+                .height(80.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+    } else {
+        AsyncImage(
+            model = image,
+            contentDescription = stringResource(R.string.product_description, name),
+            contentScale = ContentScale.FillBounds,
+            error = painterResource(id = R.drawable.ic_error),
+            modifier = modifier
+                .padding(8.dp)
+                .width(60.dp)
+                .height(80.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+    }
+}
+
+@Composable
+private fun OrderedProductInfo(
+    modifier: Modifier
+) {
+    val name = "Sari Roti Tawar"
+    name.uppercase()
+    val orderQty = 10
+    val sellPrice = 15000L
+
+    Column(modifier = modifier.padding(start = 8.dp, end = 16.dp)) {
+        Text(
+            text = name, fontWeight = FontWeight.Normal, fontSize = 14.sp,
+            style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false))
+        )
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = formatRupiah(sellPrice), fontWeight = FontWeight.Bold, fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            Spacer(modifier = modifier.width(2.dp))
+            Text(
+                text = stringResource(R.string.order_qty, orderQty),
+                fontSize = 12.sp, fontWeight = FontWeight.Bold,
+                style = TextStyle(platformStyle = PlatformTextStyle(includeFontPadding = false)),
+                color = MaterialTheme.colorScheme.tertiary
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun OrderedProductItemPreview() {
+    JJSembakoTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OrderedProductItem(
+                modifier = Modifier
+            )
+        }
+    }
+}
