6441108d8e54976a118ef7064e45dffaad10b278
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-01-29 08:24:49 +0700

    implements item retur/potong nota

--- app/src/main/java/com/dr/jjsembako/feature_history/presentation/components/screen/PotongNotaInformationContent.kt
+++ app/src/main/java/com/dr/jjsembako/feature_history/presentation/components/screen/PotongNotaInformationContent.kt
@@ -2,17 +2,36 @@ package com.dr.jjsembako.feature_history.presentation.components.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
+import com.dr.jjsembako.feature_history.presentation.components.item_list.PotongNotaItem
 
 @Composable
 fun PotongNotaInformationContent(
     modifier: Modifier
 ) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        PotongNotaItem(modifier)
+        Spacer(modifier = modifier.height(8.dp))
+        PotongNotaItem(modifier)
+        Spacer(modifier = modifier.height(8.dp))
+        PotongNotaItem(modifier)
+        Spacer(modifier = modifier.height(8.dp))
+        PotongNotaItem(modifier)
+        Spacer(modifier = modifier.height(8.dp))
+    }
 }
 
 @Composable
