559a4a8d3e8dfb099fec4244421879489278c59d
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-02-02 21:56:52 +0700

    add new fetch function OrderApiService

--- app/src/main/java/com/dr/jjsembako/core/data/remote/network/OrderApiService.kt
+++ app/src/main/java/com/dr/jjsembako/core/data/remote/network/OrderApiService.kt
@@ -3,6 +3,8 @@ package com.dr.jjsembako.core.data.remote.network
 import com.dr.jjsembako.core.data.model.OrderProduct
 import com.dr.jjsembako.core.data.remote.response.order.DeleteHandleDeleteOrderResponse
 import com.dr.jjsembako.core.data.remote.response.order.DeleteHandleDeleteProductOrderResponse
+import com.dr.jjsembako.core.data.remote.response.order.GetFetchOrderResponse
+import com.dr.jjsembako.core.data.remote.response.order.GetFetchOrdersResponse
 import com.dr.jjsembako.core.data.remote.response.order.PatchHandleUpdatePaymentStatusResponse
 import com.dr.jjsembako.core.data.remote.response.order.PatchHandleUpdateProductOrderResponse
 import com.dr.jjsembako.core.data.remote.response.order.PostHandleAddProductOrderResponse
@@ -10,11 +12,29 @@ import com.dr.jjsembako.core.data.remote.response.order.PostHandleCreateOrderRes
 import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface OrderApiService {
+
+    @GET("order")
+    suspend fun fetchOrders(
+        @Query("search") search: String? = null,
+        @Query("page") page: Int? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("minDate") minDate: String? = null,
+        @Query("maxDate") maxDate: String? = null,
+        @Query("me") me: Int? = null,
+        @Query("customerId") customerId: String? = null
+    ): GetFetchOrdersResponse
+
+    @GET("order/{id}")
+    suspend fun fetchOrder(
+        @Path("id") id: String
+    ): GetFetchOrderResponse
 
     @FormUrlEncoded
     @POST("order")
