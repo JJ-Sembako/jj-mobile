bea887f26b04c2a2275c3272cc535c73be079730
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-02-07 14:13:50 +0700

    add new function on HistoryDataSource

--- app/src/main/java/com/dr/jjsembako/feature_history/data/HistoryDataSource.kt
+++ app/src/main/java/com/dr/jjsembako/feature_history/data/HistoryDataSource.kt
@@ -2,8 +2,13 @@ package com.dr.jjsembako.feature_history.data
 
 import com.dr.jjsembako.core.common.Resource
 import com.dr.jjsembako.core.data.remote.network.OrderApiService
+import com.dr.jjsembako.core.data.remote.response.order.DeleteHandleDeleteOrderResponse
+import com.dr.jjsembako.core.data.remote.response.order.DeleteHandleDeleteProductOrderResponse
 import com.dr.jjsembako.core.data.remote.response.order.DetailOrderData
 import com.dr.jjsembako.core.data.remote.response.order.GetFetchOrderResponse
+import com.dr.jjsembako.core.data.remote.response.order.PatchHandleUpdatePaymentStatusResponse
+import com.dr.jjsembako.core.data.remote.response.order.PatchHandleUpdateProductOrderResponse
+import com.dr.jjsembako.core.data.remote.response.order.PostHandleAddProductOrderResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
@@ -46,5 +51,177 @@ class HistoryDataSource @Inject constructor(
             }
         }
     }.flowOn(Dispatchers.IO)
+
+    suspend fun handleAddProductOrder(
+        id: String,
+        productId: String,
+        amountInUnit: Int,
+        pricePerUnit: Long
+    ): Flow<Resource<out PostHandleAddProductOrderResponse>> = flow {
+        try {
+            val response =
+                orderApiService.handleAddProductOrder(id, productId, amountInUnit, pricePerUnit)
+            emit(Resource.Success(null, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        gson.fromJson(errorResponse, PostHandleAddProductOrderResponse::class.java)
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun handleUpdateProductOrder(
+        id: String,
+        productId: String,
+        amountInUnit: Int,
+        pricePerUnit: Long
+    ): Flow<Resource<out PatchHandleUpdateProductOrderResponse>> = flow {
+        try {
+            val response =
+                orderApiService.handleUpdateProductOrder(id, productId, amountInUnit, pricePerUnit)
+            emit(Resource.Success(null, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        gson.fromJson(
+                            errorResponse,
+                            PatchHandleUpdateProductOrderResponse::class.java
+                        )
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun handleUpdatePaymentStatus(id: String): Flow<Resource<out PatchHandleUpdatePaymentStatusResponse>> =
+        flow {
+            try {
+                val response =
+                    orderApiService.handleUpdatePaymentStatus(id)
+                emit(Resource.Success(null, response.message, response.statusCode))
+            } catch (e: CancellationException) {
+                // Do nothing, the flow is cancelled
+            } catch (e: IOException) {
+                // Handle IOException (connection issues, timeout, etc.)
+                emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+            } catch (e: Exception) {
+                if (e is HttpException) {
+                    val errorResponse = e.response()?.errorBody()?.string()
+                    val statusCode = e.code()
+                    val errorMessage = e.message()
+
+                    if (errorResponse != null) {
+                        val errorResponseObj =
+                            gson.fromJson(
+                                errorResponse,
+                                PatchHandleUpdatePaymentStatusResponse::class.java
+                            )
+                        emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                    } else {
+                        emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                    }
+                } else {
+                    emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+                }
+            }
+        }.flowOn(Dispatchers.IO)
+
+    suspend fun handleDeleteProductOrder(
+        id: String,
+        productId: String
+    ): Flow<Resource<out DeleteHandleDeleteProductOrderResponse>> = flow {
+        try {
+            val response =
+                orderApiService.handleDeleteProductOrder(id, productId)
+            emit(Resource.Success(null, response.message, response.statusCode))
+        } catch (e: CancellationException) {
+            // Do nothing, the flow is cancelled
+        } catch (e: IOException) {
+            // Handle IOException (connection issues, timeout, etc.)
+            emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val errorResponse = e.response()?.errorBody()?.string()
+                val statusCode = e.code()
+                val errorMessage = e.message()
+
+                if (errorResponse != null) {
+                    val errorResponseObj =
+                        gson.fromJson(
+                            errorResponse,
+                            DeleteHandleDeleteProductOrderResponse::class.java
+                        )
+                    emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                } else {
+                    emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                }
+            } else {
+                emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun handleDeleteOrder(id: String): Flow<Resource<out DeleteHandleDeleteOrderResponse>> =
+        flow {
+            try {
+                val response =
+                    orderApiService.handleDeleteOrder(id)
+                emit(Resource.Success(null, response.message, response.statusCode))
+            } catch (e: CancellationException) {
+                // Do nothing, the flow is cancelled
+            } catch (e: IOException) {
+                // Handle IOException (connection issues, timeout, etc.)
+                emit(Resource.Error("Masalah jaringan koneksi internet", 408, null))
+            } catch (e: Exception) {
+                if (e is HttpException) {
+                    val errorResponse = e.response()?.errorBody()?.string()
+                    val statusCode = e.code()
+                    val errorMessage = e.message()
+
+                    if (errorResponse != null) {
+                        val errorResponseObj =
+                            gson.fromJson(
+                                errorResponse,
+                                DeleteHandleDeleteOrderResponse::class.java
+                            )
+                        emit(Resource.Error(errorResponseObj.message, statusCode, null))
+                    } else {
+                        emit(Resource.Error(errorMessage ?: "Unknown error", statusCode, null))
+                    }
+                } else {
+                    emit(Resource.Error(e.message ?: "Unknown error", 400, null))
+                }
+            }
+        }.flowOn(Dispatchers.IO)
 
 }
