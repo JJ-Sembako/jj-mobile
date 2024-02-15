2cb0fbbd1a7acbe02457457745fdaea7913b7e78
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-02-15 22:01:25 +0700

    implements order cust history & delete bottom sheet

--- app/src/main/java/com/dr/jjsembako/feature_customer/presentation/detail/DetailPelangganScreen.kt
+++ app/src/main/java/com/dr/jjsembako/feature_customer/presentation/detail/DetailPelangganScreen.kt
@@ -1,22 +1,18 @@
 package com.dr.jjsembako.feature_customer.presentation.detail
 
+import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
@@ -37,13 +33,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
@@ -52,37 +49,38 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.dr.jjsembako.R
 import com.dr.jjsembako.core.common.StateResponse
-import com.dr.jjsembako.core.data.model.FilterOption
 import com.dr.jjsembako.core.data.remote.response.customer.DataCustomer
 import com.dr.jjsembako.core.data.remote.response.order.OrderDataItem
-import com.dr.jjsembako.core.presentation.components.bottom_sheet.BottomSheetOrder
+import com.dr.jjsembako.core.presentation.components.bottom_sheet.BottomSheetOrderHistory
 import com.dr.jjsembako.core.presentation.components.card.CustomerInfo
+import com.dr.jjsembako.core.presentation.components.card.OrderHistoryCard
 import com.dr.jjsembako.core.presentation.components.dialog.AlertDeleteDialog
 import com.dr.jjsembako.core.presentation.components.dialog.AlertErrorDialog
 import com.dr.jjsembako.core.presentation.components.dialog.LoadingDialog
 import com.dr.jjsembako.core.presentation.components.screen.ErrorScreen
 import com.dr.jjsembako.core.presentation.components.screen.LoadingScreen
+import com.dr.jjsembako.core.presentation.components.screen.NotFoundScreen
 import com.dr.jjsembako.core.presentation.components.utils.SearchFilter
 import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
+import com.dr.jjsembako.core.utils.DataMapper.mapOrderDataItemToDataOrderHistoryCard
+import com.dr.jjsembako.core.utils.initializeDateValues
 import com.dr.jjsembako.feature_customer.presentation.components.CustomerButtonInfo
 import eu.bambooapps.material3.pullrefresh.PullRefreshIndicator
 import eu.bambooapps.material3.pullrefresh.pullRefresh
 import eu.bambooapps.material3.pullrefresh.rememberPullRefreshState
-import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
 @Composable
 fun DetailPelangganScreen(
     idCust: String,
+    context: Context,
+    clipboardManager: ClipboardManager,
+    onNavigateToDetail: (String) -> Unit,
     onNavigateBack: () -> Unit,
     onNavigateToEditCust: () -> Unit,
     openMaps: (String) -> Unit,
@@ -96,8 +94,6 @@ fun DetailPelangganScreen(
     val statusCode = viewModel.statusCode.observeAsState().value
     val message = viewModel.message.observeAsState().value
     val customerData = viewModel.customerData
-    val orderPagingItems: LazyPagingItems<OrderDataItem> =
-        viewModel.orderState.collectAsLazyPagingItems()
 
     // Set id for the first time Composable is rendered
     LaunchedEffect(idCust) {
@@ -136,6 +132,9 @@ fun DetailPelangganScreen(
                 DetailPelangganContent(
                     cust = customerData,
                     viewModel = viewModel,
+                    context = context,
+                    clipboardManager = clipboardManager,
+                    onNavigateToDetail = { id -> onNavigateToDetail(id) },
                     onNavigateBack = { onNavigateBack() },
                     onNavigateToEditCust = { onNavigateToEditCust() },
                     openMaps = { url -> openMaps(url) },
@@ -155,6 +154,9 @@ fun DetailPelangganScreen(
 private fun DetailPelangganContent(
     cust: DataCustomer,
     viewModel: DetailPelangganViewModel,
+    context: Context,
+    clipboardManager: ClipboardManager,
+    onNavigateToDetail: (String) -> Unit,
     onNavigateBack: () -> Unit,
     onNavigateToEditCust: () -> Unit,
     openMaps: (String) -> Unit,
@@ -168,40 +170,45 @@ private fun DetailPelangganContent(
     val statusCode = viewModel.statusCode.observeAsState().value
     val message = viewModel.message.observeAsState().value
     val isRefreshing by viewModel.isRefreshing.collectAsState(initial = false)
+    val orderPagingItems: LazyPagingItems<OrderDataItem> =
+        viewModel.orderState.collectAsLazyPagingItems()
 
     val focusManager = LocalFocusManager.current
     val keyboardController = LocalSoftwareKeyboardController.current
-    val scrollState = rememberScrollState()
-    val coroutineScope = rememberCoroutineScope()
-    val keyboardHeight = WindowInsets.ime.getBottom(LocalDensity.current)
 
     val showLoadingDialog = rememberSaveable { mutableStateOf(false) }
     val showErrorDialog = rememberSaveable { mutableStateOf(false) }
-
     var menuExpanded by remember { mutableStateOf(false) }
     val showDialog = remember { mutableStateOf(false) }
     val showSheet = remember { mutableStateOf(false) }
-    val (selectedOptionPayment, onOptionSelectedPayment) = remember {
-        mutableStateOf(
-            radioOptionsPayment[0]
-        )
-    }
-    val (selectedOptionOrder, onOptionSelectedOrder) = remember { mutableStateOf(radioOptionsOrder[0]) }
-    val searchQuery = remember { mutableStateOf("") }
+    val isFilterOn = rememberSaveable { mutableStateOf(false) }
+    val searchQuery = rememberSaveable { mutableStateOf("") }
     val activeSearch = remember { mutableStateOf(false) }
+    val fromDate = rememberSaveable { mutableStateOf("") }
+    val untilDate = rememberSaveable { mutableStateOf("") }
     val pullRefreshState = rememberPullRefreshState(
         refreshing = isRefreshing,
         onRefresh = { viewModel.refresh() })
 
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.anim_empty))
-    val progress by animateLottieCompositionAsState(
-        composition,
-        iterations = LottieConstants.IterateForever,
-    )
+    LaunchedEffect(Unit) {
+        initializeDateValues(fromDate, untilDate)
+    }
 
-    LaunchedEffect(key1 = keyboardHeight) {
-        coroutineScope.launch {
-            scrollState.scrollBy(keyboardHeight.toFloat())
+    LaunchedEffect(searchQuery) {
+        if (searchQuery.value.isNotEmpty()) {
+            if (isFilterOn.value) viewModel.fetchOrders(
+                searchQuery.value,
+                fromDate.value,
+                untilDate.value
+            )
+            else viewModel.fetchOrders(searchQuery.value, null, null)
+        } else {
+            if (isFilterOn.value) viewModel.fetchOrders(
+                null,
+                fromDate.value,
+                untilDate.value
+            )
+            else viewModel.fetchOrders(null, null, null)
         }
     }
 
@@ -313,7 +320,6 @@ private fun DetailPelangganContent(
             )
             Column(
                 modifier = modifier
-                    .verticalScroll(scrollState)
                     .fillMaxSize()
                     .clickable(
                         indication = null,
@@ -355,21 +361,70 @@ private fun DetailPelangganContent(
                     modifier = modifier
                 )
                 Spacer(modifier = modifier.height(16.dp))
-                LottieAnimation(
-                    enableMergePaths = true,
-                    composition = composition,
-                    progress = { progress },
-                    modifier = modifier.size(150.dp)
-                )
+
+                when (orderPagingItems.loadState.refresh) {
+                    LoadState.Loading -> {
+                        LoadingScreen(modifier = modifier)
+                    }
+
+                    is LoadState.Error -> {
+                        val error = orderPagingItems.loadState.refresh as LoadState.Error
+                        Log.e(tag, "Error")
+                        ErrorScreen(
+                            onNavigateBack = { },
+                            onReload = {
+                                if (searchQuery.value.isNotEmpty()) {
+                                    if (isFilterOn.value) viewModel.fetchOrders(
+                                        searchQuery.value,
+                                        fromDate.value,
+                                        untilDate.value
+                                    )
+                                    else viewModel.fetchOrders(searchQuery.value, null, null)
+                                } else {
+                                    if (isFilterOn.value) viewModel.fetchOrders(
+                                        null,
+                                        fromDate.value,
+                                        untilDate.value
+                                    )
+                                    else viewModel.fetchOrders(null, null, null)
+                                }
+                            },
+                            message = error.error.localizedMessage ?: "Unknown error",
+                            showButtonBack = false,
+                            modifier = modifier
+                        )
+                    }
+
+                    else -> {
+                        if (orderPagingItems.itemCount > 0) {
+                            LazyColumn(
+                                modifier = modifier
+                                    .fillMaxWidth()
+                            ) {
+                                items(orderPagingItems.itemCount) { index ->
+                                    OrderHistoryCard(
+                                        data = mapOrderDataItemToDataOrderHistoryCard(
+                                            orderPagingItems[index]!!
+                                        ),
+                                        context = context,
+                                        onNavigateToDetail = { id -> onNavigateToDetail(id) },
+                                        clipboardManager = clipboardManager,
+                                        modifier = modifier
+                                    )
+                                }
+                            }
+                        } else {
+                            NotFoundScreen(modifier = modifier)
+                        }
+                    }
+                }
+
                 Spacer(modifier = modifier.height(48.dp))
                 if (showSheet.value) {
-                    BottomSheetOrder(
-                        optionListPayment = radioOptionsPayment,
-                        optionListOrder = radioOptionsOrder,
-                        selectedOptionPayment = selectedOptionPayment,
-                        selectedOptionOrder = selectedOptionOrder,
-                        onOptionSelectedPayment = onOptionSelectedPayment,
-                        onOptionSelectedOrder = onOptionSelectedOrder,
+                    BottomSheetOrderHistory(
+                        fromDate = fromDate,
+                        untilDate = untilDate,
+                        isFilterOn = isFilterOn,
                         showSheet = showSheet,
                         modifier = modifier
                     )
@@ -398,26 +453,15 @@ private fun DetailPelangganContent(
     }
 }
 
-private val radioOptionsPayment = listOf(
-    FilterOption("Semua", "semua"),
-    FilterOption("Belum Lunas", "belum-lunas"),
-    FilterOption("Lunas", "lunas")
-)
-
-private val radioOptionsOrder = listOf(
-    FilterOption("Semua", "semua"),
-    FilterOption("Menunggu Konfirmasi", "menunggu-konfirmasi"),
-    FilterOption("Dikemas", "dikemas"),
-    FilterOption("Dikirim", "dikirim"),
-    FilterOption("Selesai", "selesai")
-)
-
 @Composable
 @Preview(showBackground = true)
 private fun DetailPelangganScreenPreview() {
     JJSembakoTheme {
         DetailPelangganScreen(
             idCust = "",
+            context = LocalContext.current,
+            clipboardManager = LocalClipboardManager.current,
+            onNavigateToDetail = {},
             onNavigateBack = {},
             onNavigateToEditCust = {},
             openMaps = {},
