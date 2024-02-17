70c5a1b11ae2d35a1a2a378dd5da7da35f8fcfeb
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-02-17 10:15:13 +0700

    reformat pull refresh

--- app/src/main/java/com/dr/jjsembako/feature_history/presentation/add_product_order/TambahBarangPesananScreen.kt
+++ app/src/main/java/com/dr/jjsembako/feature_history/presentation/add_product_order/TambahBarangPesananScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -42,7 +41,6 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dr.jjsembako.R
 import com.dr.jjsembako.core.common.StateResponse
@@ -55,7 +53,6 @@ import com.dr.jjsembako.core.presentation.theme.JJSembakoTheme
 import eu.bambooapps.material3.pullrefresh.PullRefreshIndicator
 import eu.bambooapps.material3.pullrefresh.pullRefresh
 import eu.bambooapps.material3.pullrefresh.rememberPullRefreshState
-import kotlin.math.roundToInt
 
 @Composable
 fun TambahBarangPesananScreen(
@@ -290,12 +287,11 @@ private fun TambahBarangPesananContent(
                     )
                 }
             }
+
             PullRefreshIndicator(
                 refreshing = isRefreshing,
                 state = pullRefreshState,
-                modifier = modifier
-                    .fillMaxWidth()
-                    .height((pullRefreshState.progress * 100).roundToInt().dp)
+                modifier = modifier.align(Alignment.TopCenter)
             )
         }
 
