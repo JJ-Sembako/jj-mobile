0314d0c9e38f8ba4e25378fc0d8b6ec6a70a209a
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-02-27 20:19:07 +0700

    add visibility omzet

--- app/src/main/java/com/dr/jjsembako/feature_home/presentation/components/OmzetSection.kt
+++ app/src/main/java/com/dr/jjsembako/feature_home/presentation/components/OmzetSection.kt
@@ -1,14 +1,24 @@
 package com.dr.jjsembako.feature_home.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,6 +29,11 @@ import com.dr.jjsembako.core.utils.formatRupiah
 
 @Composable
 fun OmzetSection(omzet: Long = 0, isErrorInit: Boolean = false, modifier: Modifier) {
+    val omzetVisibility = rememberSaveable { mutableStateOf(false) }
+    val icon =
+        if (omzetVisibility.value) painterResource(id = R.drawable.ic_visibility_on)
+        else painterResource(id = R.drawable.ic_visibility_off)
+
     Card(
         modifier = modifier
             .fillMaxWidth()
@@ -31,9 +46,31 @@ fun OmzetSection(omzet: Long = 0, isErrorInit: Boolean = false, modifier: Modifi
             Text(
                 text = stringResource(R.string.turnover_this_month), fontWeight = FontWeight.Bold
             )
-            if (isErrorInit){
+            if (isErrorInit) {
                 Text(text = stringResource(R.string.error_string))
-            } else Text(text = formatRupiah(omzet))
+            } else {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (omzetVisibility.value) {
+                        Text(text = formatRupiah(omzet))
+                    } else {
+                        Text(text = stringResource(R.string.hidden_value))
+                    }
+                    IconButton(
+                        onClick = { omzetVisibility.value = !omzetVisibility.value },
+                        modifier = modifier.padding(start = 8.dp)
+                    ) {
+                        Icon(
+                            painter = icon, modifier = modifier.size(24.dp),
+                            tint = MaterialTheme.colorScheme.onPrimary,
+                            contentDescription = stringResource(R.string.change_visibility_omzet)
+                        )
+                    }
+                }
+            }
         }
     }
 }
